d90d165db1f6869d0db9694a17ac5d34a6db24c3
Baboucarr Sanneh <basanneh@gmail.com>
2020-09-27 14:41:15 -0400

    Added new field to the User class

--- src/backend/authapp/models.py
+++ src/backend/authapp/models.py
@@ -2,11 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+STATUS = (
+    (0,"Admin"),
+    (1, "Doctor"),
+    (2, "Patient")
+)
 
 class User(AbstractUser):
     #email = models.EmailField(verbose_name='email', max_length=255, unique=True),
     phone = models.CharField(null=True, max_length=255)
-    REQUIRED_FIELDS = ['first_name','last_name','email','phone']
+    user_role = models.IntegerField(choices=STATUS, default=2)
+    REQUIRED_FIELDS = ['first_name','last_name','email','phone','user_role']
     #USERNAME_FIELD = 'email'
 
 #     def get_username(self):
