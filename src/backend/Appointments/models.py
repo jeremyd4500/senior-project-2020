1a660dffd101496c83c81ad6a20a0e703d6fab5e
Baboucarr Sanneh <basanneh@gmail.com>
2020-10-21 20:56:31 -0400

    Made changes to the Appointment Model

--- src/backend/Appointments/models.py
+++ src/backend/Appointments/models.py
@@ -6,30 +6,32 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Appointment(models.Model):
     GENDER =(
-        ('M', 'Male'),
-        ('F', 'Female'),
+        (0, 'Male'),
+        (1, 'Female'),
     )
 
     STATUS = (
-        (0,'Booked'),
-        (1,'No Show'),
-        (2,'Seen')
+        (0,'Pending'),
+        (1,'Schedule'),
+        (2,'No show'),
+        (3, 'Completed')
     )
-    FirstName = models.CharField(max_length=30)
-    LastName = models.CharField(max_length=30)
-    Age = models.IntegerField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
     email = models.EmailField(max_length=254, blank=True)
-    Date = models.DateField(auto_now=True)
+    date = models.DateField(auto_now=True)
     appointment_date = models.DateField(blank=True)
     #user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    Time = models.TimeField(blank=True)
-    Gender = models.CharField(max_length=1, choices=GENDER)
+    doctor_id = models.IntegerField(blank=True, null=True)
+    time = models.TimeField(blank=True)
+    gender = models.IntegerField(default=0, choices=GENDER)
     status = models.IntegerField(choices=STATUS, default=0)
     description = models.TextField()
 
     def __str__(self):
-        return (self.FirstName +" " +self.LastName)
+        return (self.first_name +" " +self.last_name)
 
 
 
