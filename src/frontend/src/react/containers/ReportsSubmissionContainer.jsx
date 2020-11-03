abf442ccd5bf269c228fd9d1fdd1ce95b344bed9
dtiglao <54993600+dtiglao@users.noreply.github.com>
2020-11-03 14:16:53 -0500

    Added two new pages to go along with Reports
    ReportsDoctorView and ReportsSubmissionContainer are meant to allow the doctor to view the page with more information and editorial tools. Submission is meant for submitting the page.

---
+++ src/frontend/src/react/containers/ReportsSubmissionContainer.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ContainerView from 'react/components/ContainerView';
+import ModuleLayout from 'react/components/ModuleLayout';
+
+class ReportsSubmissionContainer extends Component {
+	render() {
+		//...
+
+		return (
+			<ModuleLayout hasHeader>
+				{/*<ContainerView>
+					{/*<div className='ReportsContainer'>
+						<h1>Hello Ok</h1>
+		</div>
+				</ContainerView>*/}
+
+			</ModuleLayout>
+		);
+	}
+}
+
+export default ReportsSubmissionContainer;
